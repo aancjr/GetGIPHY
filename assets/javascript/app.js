13cd4bc1aa15a0e0e1c19949d0fbed14fa4b30f1
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-28 08:27:27 -0400

    created buttons for user to start with. shows on HTML page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,31 @@
 //variable for the api query
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=vokt7stTYaqiAx2gkIYLxGMTgFfRtAT3&limit=10";
 
-var makesModels = ["BMW", "Mercedes", "Ford", "Chevy", "Supra", "m3"]
+//array to provide the user with recommended GIFS...
+var makesModels = ["BMW", "Mercedes", "Ford", "Chevy", "Supra", "m3"];
 
-//ajax call out to the api...
-$.ajax ({
-    url: queryURL,
-    method: "GET"
-}).then (function(response){
-    console.log(response);
-});
+//function to show recommended GIF buttons
+function buttons() {
+    //delete listed GIFS prior to adding new buttons
+    $("#buttons").empty();
+    
+    //loop through the array of makes and models...
+    var i;
+    for (i = 0; i < makesModels.length; i++) {
+        $("#buttons").append("<button>" + makesModels[i]);
+    }
+}
+
+$(function (){
+
+    //ajax call out to the api...
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then (function(response){
+        console.log(response);
+    });
+    
+    //call buttons function to show list of gifs to start with...
+    buttons();
+})
