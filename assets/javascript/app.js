cfb92f28321f44c34767b520053edb64ecf98619
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-28 11:18:04 -0400

    adjusted layout of some of the jquery to fit instide proper functions. removed loop in AJAX call. Working on getting gif URL to show. URL not populating in console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,65 @@
-//variable for the api query
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="+request+"&api_key=vokt7stTYaqiAx2gkIYLxGMTgFfRtAT3&limit=10";
-var request = $("#buttons").val();
 
 //array to provide the user with recommended GIFS...
 var makesModels = ["BMW", "Mercedes", "Ford", "Chevy", "Supra", "m3"];
 
-//function to show recommended GIF buttons
-function buttons() {
-    //delete listed GIFS prior to adding new buttons
-    $("#buttons").empty();
 
-    //loop through the array of makes and models...
-    var i;
-    for (i = 0; i < makesModels.length; i++) {
-        $("#buttons").append("<button>" + makesModels[i]);
-    }
-}
-
-$(function (){
+$(function displayGif() {
+    //variable for the api query
+    var request = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+request+"&api_key=vokt7stTYaqiAx2gkIYLxGMTgFfRtAT3&limit=10";
+    
     //ajax call out to the api...
     $.ajax ({
         url: queryURL,
         method: "GET"
     }).then (function(response){
-        var results = response.data;
-        for (var i = 0; i < results.length; i++){
             //creating a div to hold the gif in the right area of HTML
-            var gifDiv = $("#gifHolder").append("<div>");
+            var gifDiv = $("<div class='gif-div'>");
             //storing the gif url
             var gifURL = response.data.url;
             //element to show gif
             var showGif = $("<img>").attr("src", gifURL);
             //display gif
             gifDiv.append(showGif);
-            };
-        });
-        
+            console.log(gifURL)
+    });
     
-    //function to take user input and add it as a button...
+    //function to show recommended GIF buttons
+    function createButton() {
+        //delete listed GIFS prior to adding new buttons
+        $("#buttons").empty();
+        
+        //loop through the array of makes and models...
+        var i;
+        for (i = 0; i < makesModels.length; i++) {
+            //create new button
+            var button = $("<button>");
+            //give each new div a class so that we can call all buttons later
+            button.addClass("gif-btn");
+            //add a data-attribute
+            button.attr("data-name", makesModels[i]);
+            //give button text
+            button.text(makesModels[i]);
+            //add button to button view
+            $("#buttons").append(button);
+        }
+    }
+ 
+    //function handles events when button is clicked...
     $("#add-makesModels").on("click", function(event){
         event.preventDefault();
         //variable to grab the users text...
-        var input = $("#userInput").val();
+        var request = $("#userInput").val();
         //take users text and add it to the array of makesModels
-        makesModels.push(input);
+        makesModels.push(request);
         //call the buttons function to run through the array and creat a button.
-        buttons();
+        createButton();
     })
-    //call buttons function to show list of gifs to start with...
-    buttons();
 
+    //Add click event listener to all alements with "gif-btn"...
+    $(document).on("click", ".gif-btn", displayGif);
+    
+    //call buttons function to show list of gifs buttons...
+   createButton();
+    
 });
